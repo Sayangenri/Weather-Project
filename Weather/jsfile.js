92fb4889c8fb489a1cb50d803ddff0d1ba1bc23b
Sayangenri <137492664+Sayangenri@users.noreply.github.com>
2023-06-30 21:24:45 +0530

    Add files via upload

---
+++ Weather/jsfile.js
@@ -0,0 +1,30 @@
+const API_KEY = `3265874a2c77ae4a04bb96236a642d2f`
+const form = document.querySelector("form")
+const search = document.querySelector("#searchbox")
+const weather = document.querySelector("#weather")
+    // const API = `https://api.openweathermap.org/data/2.5/weather?
+    // q=${city}&appid=${API_KEY}&units=metric`
+    // const IMG_URL = `https: //openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+const getWeather = async(city) => {
+    weather.innerHTML = `<h2> Loading... <h2>`
+   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+    const response = await fetch(url);
+    const data = await response.json()
+    return showWeather(data)
+}
+
+const showWeather = (data) => {
+    weather.innerHTML = `
+            <h2>${data.main.temp} ℃</h2>
+            <h4> ${data.weather[0].main} </h4>
+        </div>
+    `
+}
+
+form.addEventListener(
+    "submit",
+    function(event) {
+        getWeather(search.value)
+        event.preventDefault();
+    }
+)
